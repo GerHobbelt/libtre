487da93d44bb94555a813f313fcb89e88942d913
Ger Hobbelt <ger@hobbelt.com>
2011-06-29 19:10:30 +0200

    debug print code: print a pointer as %p, not as integer! (that sort of thing only works well on LP64 platforms and Windows/MSVC is NOT LP64 but LLP64)

--- lib/tre-parse.c
+++ lib/tre-parse.c
@@ -457,8 +457,8 @@ tre_parse_bracket(tre_parse_ctx_t *ctx, tre_ast_node_t **result)
       min = l->code_min;
       max = l->code_max;
 
-      DPRINT(("item: %d - %d, class %ld, curr_max = %d\n",
-	      (int)l->code_min, (int)l->code_max, (long)l->u.classt, curr_max));
+      DPRINT(("item: %d - %d, class %p, curr_max = %d\n",
+	      (int)l->code_min, (int)l->code_max, (void *)l->u.classt, curr_max));
 
       if (negate)
 	{
